86181896812d8ab61abb0be60969bad9181796dc
Black  Alpha <148836568+CharukaMahesh@users.noreply.github.com>
2024-09-23 13:54:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/CharukaMahesh/song-bot/refs/heads/main/IMAGES/20240923_114717.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "",
 };
